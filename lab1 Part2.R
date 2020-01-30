54cb1b1b9c5b10886b4a2c85093d9c864cb939ee
YichenZhang686688 <54687154+YichenZhang686688@users.noreply.github.com>
2020-01-30 11:50:37 -0500

    Add files via upload

---
+++ lab1 Part2.R
@@ -0,0 +1,59 @@
+multivariate <- read.csv(file.choose(), header = TRUE)
+help("read.csv")
+head(multivariate)
+attach(multivariate)
+help(lm)
+mm <-lm(Homeowners~Immigrant)
+summary(mm)
+
+plot(Homeowners~Immigrant)
+help(abline)
+abline(mm)
+abline(mm,col=2,lwd=3)
+newImmigrantdata <- data.frame(Immigrant = c(0,  20))
+mm %>% predict(newImmigrantdata)
+abline(mm)
+abline(mm,col=3,lwd=3) 
+attributes(mm)
+mm$coefficients
+
+#creating polts
+#chapter2--R Graphics Cookbook
+plot(mtcars$wt,mtcars$mpg)
+install.packages("ggplot2")
+library(ggplot2)
+qplot(mtcars$wt,mtcars$mpg)
+qplot(wt,mpg,data=mtcars)
+ggplot(mtcars,aes(x=wt,y=mpg))+geom_point()
+plot(pressure$temperature,pressure$pressure, type="l")
+points(pressure$temperature,pressure$pressure)
+
+lines(pressure$temperature,pressure$pressure/2,col="red")
+points(pressure$temperature,pressure$pressure/2,col="blue")
+library(ggplot2)
+qplot(pressure$temperature,pressure$pressure, geom="line")
+qplot(temperature,pressure,data=pressure, geom="line")
+ggplot(pressure,aes(x=temperature,y=pressure))+geom_line()+geom_point()
+ggplot(pressure,aes(x=temperature,y=pressure))+geom_line()+geom_point()
+
+#creating bar grapgs
+barplot(BOD$demand,names.arg=BOD$Time)
+table(mtcars$cyl)
+barplot(table(mtcars$cyl))
+qplot(mtcars$cyl)
+qplot(factor(mtcars$cyl))
+qplot(factor(cyl),data=mtcars)
+ggplot(mtcars,aes(x=factor(cyl)))+geom_bar
+
+hist(mtcars$mpg)
+hist(mtcars$mpg,breaks  =10)
+hist(mtcars$mpg,breaks  =5)
+hist(mtcars$mpg,breaks  =12)
+qplot(mpg,data = mtcars,binwidth=4)
+ggplot(mtcars,aes(x=mpg))+geom_histogram(binwidth=4)
+ggplot(mtcars,aes(x=mpg))+geom_histogram(binwidth=5)
+
+plot(ToothGrowth$supp,ToothGrowth$len)
+boxplot(len~supp,data=ToothGrowth)
+
+
